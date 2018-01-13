c136913bae1c6ab72e16491dea6ba11a6b6ba7e4
mo <mohamoud51@hotmail.com>
2018-01-12 22:25:09 -0500

    i

---
+++ AuthIDPserver/Config.cs
@@ -0,0 +1,73 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthIDPserver
+{
+    public static class Config
+    {
+        //1
+        public static List<TestUser> GetUsers()
+        {
+            //returns users
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId="1001",
+                    Username="Frank",
+                    Password="password",
+
+                    Claims= new List<Claim>
+                    {
+                        new Claim("given_name","Frank"),
+                        new Claim("family_name","Underwood"),
+                    }
+
+                },//user ends
+
+                 new TestUser
+                {
+                    SubjectId="1002",
+                    Username="Claire",
+                    Password="password",
+
+                    Claims= new List<Claim>
+                    {
+                        new Claim("given_name","Claire"),
+                        new Claim("family_name","Underwood"),
+
+                    }
+
+                }//user ends
+            };//list ends
+
+        }
+
+        //2 not sure scope           
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            //returns this 
+            //maps to subjectId ensures its ok
+            return new List<IdentityResource> {
+
+                new IdentityResources.OpenId(),
+
+                new IdentityResources.Profile()
+
+            };
+        }
+
+        //3 clients
+        public static IEnumerable<Client> GetClients()
+        {
+            //ret empty list for know
+            return new List<Client>();
+        }
+
+    }
+}
--- AuthIDPserver/Startup.cs
+++ AuthIDPserver/Startup.cs
@@ -23,17 +23,36 @@ namespace AuthIDPserver
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+            //add indentity server
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(Config.GetUsers())//<---gets from static method in Config class
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())//<-method in Config class
+                .AddInMemoryClients(Config.GetClients());//--Config getClients()
+
+
+
+            //mvc
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory lf)
         {
+            //loggerfactory
+            lf.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //call iden4
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
             app.UseMvc();
         }
     }
